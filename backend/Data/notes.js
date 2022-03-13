5aff0d45d0e40a8ee2c2c14a0d669d6a74c77f7a
Madhumita Saha <madsaha@deloitte.com>
2022-03-13 23:49:08 +0530

    Initialised frontend and backend, pushing into git

---
+++ backend/Data/notes.js
@@ -0,0 +1,22 @@
+const notes = [
+  {
+    _id: "1",
+    title: "Hello World1",
+    content: "This is my first note",
+    category: "General",
+  },
+  {
+    _id: "2",
+    title: "Hello World2",
+    content: "This is my second note",
+    category: "General",
+  },
+  {
+    _id: "3",
+    title: "Hello World3",
+    content: "This is my third note",
+    category: "General",
+  },
+];
+
+module.exports = notes;
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const notes = require("./Data/notes");
+const dotenv = require("dotenv");
+const { addAbortSignal } = require("stream");
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
