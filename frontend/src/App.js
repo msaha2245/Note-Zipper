05514740cde97a1c22a41fb21cc365750799ba76
Madhumita Saha <madsaha@deloitte.com>
2022-03-14 02:04:20 +0530

    Added landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
-
-const App = () => <h1 className="App">Hello World</h1>;
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import LandingPage from "./screens/LandingPage/LandingPage";
+const App = () => (
+  <>
+    <Header />
+    <main>
+      <LandingPage />
+    </main>
+    <Footer />
+  </>
+);
 
 export default App;
